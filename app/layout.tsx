5b6f210dbbf5c25ddc217509c72012148f74ce3c
axchisan <leftyrancuentabot@gmail.com>
2025-08-04 09:57:46 -0500

    agregada la carpeta de public al dockerfile

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Axchisan',
+  title: 'AxchiSan',
   description: 'Created with Axchisan',
   generator: 'Axchisan.dev',
 }
